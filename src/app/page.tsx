533048f40e60bad528262400ff83cac532f0ccc5
David <66641216+fhswno@users.noreply.github.com>
2023-03-17 17:58:46 +0000

    Slight setup tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Follow the Quest with{" "}
+          <b>
+            <code>#OFFQUEST</code>
+          </b>{" "}
+          on Twitter
         </p>
         <div>
           <a
@@ -18,7 +21,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -87,5 +90,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
+    extend: {
+      // Add custom theme configurations here
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
