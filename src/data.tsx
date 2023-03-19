6d7587bcd0797be1e21bb2ab99d9a51a38ba2904
David <66641216+fhswno@users.noreply.github.com>
2023-03-19 02:30:17 +0000

    Mock data filke

---
+++ src/data.tsx
@@ -0,0 +1,44 @@
+export const CardData = [
+  {
+    title: "Open Source",
+    description:
+      "We believe that open source is key to unlocking innovation, that's why we embrace open-source technology, making our products accessible to anyone who wants to contribute.",
+    icon: "code-branch",
+    color: "blue",
+  },
+  {
+    title: "Disruptive Innovation",
+    description:
+      "We don't just create new products. We redefine entire industries. We think outside the box, dare everything, test our hypotheses, and perform extensive user research to solve real, complex problems.",
+    icon: "lightbulb",
+    color: "red",
+  },
+  {
+    title: "Beautiful Design",
+    description:
+      "We approach design with the same level of card and attention to detail as we do with our technology. Our products are not only beautiful, but also effortless and intuitive, providing our community with a delightful experience, that is both functional and enjoyable.",
+    icon: "paint-brush",
+    color: "green",
+  },
+  {
+    title: "Trusted & Transparent",
+    description:
+      "We believe that technology should be trustworthy, reliable and secure. We follow the most stringent security protocols, take user privacy very seriously, and disclose all of our processes and practices in a transparent manner.",
+    icon: "lock",
+    color: "yellow",
+  },
+  {
+    title: "Sound Financials",
+    description:
+      "We believe that the key to success is innovation. We don't create products just to make money; we make money to build better products. Our focus is on creating the best possible experiences for our community, and we believe that if we do that well, profits will follow. We will never sacrifice our values for short-term gains.",
+    icon: "pound-sign",
+    color: "purple",
+  },
+  {
+    title: "Community Driven",
+    description:
+      "We believe that our community is the most important part of our business. We are committed to building a community of like-minded individuals, who share our values and are passionate about our mission. We are always looking for new ways to engage with our community, and we are always open to feedback and suggestions.",
+    icon: "users",
+    color: "orange",
+  },
+];
