0d3e4b5f7f06208e4ec9f596063f2ff08f6b3576
David <66641216+fhswno@users.noreply.github.com>
2023-03-19 02:29:54 +0000

    Tailwind configs for responsive breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   theme: {
     extend: {
       // Add custom theme configurations here
+      fontFamily: {
+        "source-code-pro": ["Source Code Pro", "monospace"],
+      },
+      screens: {
+        mobile: "550px",
+
+        tablet: "840px",
+
+        desktop: "1076px",
+      },
     },
   },
   variants: {
