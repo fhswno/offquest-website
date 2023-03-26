74758c683764eec97f11f1d10b28b6e3c1a1c1e5
David <66641216+fhswno@users.noreply.github.com>
2023-03-26 19:01:16 +0100

    Minor copy tweaks

--- src/data.tsx
+++ src/data.tsx
@@ -2,21 +2,21 @@ export const CardData = [
   {
     title: "Open Source",
     description:
-      "We believe that open source is key to unlocking innovation, that's why we embrace open-source technology, making our products accessible to anyone who wants to contribute.",
+      "We believe that open source is key to unlocking innovation, that's why we embrace open-source technology, making our products accessible to anyone willing to contribute.",
     icon: "code-branch",
     color: "blue",
   },
   {
     title: "Disruptive Innovation",
     description:
-      "We don't just create new products. We redefine entire industries. We think outside the box, dare everything, test our hypotheses, and perform extensive user research to solve real, complex problems.",
+      "We don't just create new products. We redefine entire industries. We think outside the box, dare everything, test our hypotheses, perform extensive user research to solve real, complex problems.",
     icon: "lightbulb",
     color: "red",
   },
   {
     title: "Beautiful Design",
     description:
-      "We approach design with the same level of card and attention to detail as we do with our technology. Our products are not only beautiful, but also effortless and intuitive, providing our community with a delightful experience, that is both functional and enjoyable.",
+      "We approach design with the same level of care and attention to detail as we do with our technology. Our products are not only beautiful, but also effortless and intuitive, providing our community with a delightful experience, that is both functional and enjoyable.",
     icon: "paint-brush",
     color: "green",
   },
@@ -30,7 +30,7 @@ export const CardData = [
   {
     title: "Sound Financials",
     description:
-      "We believe that the key to success is innovation. We don't create products just to make money; we make money to build better products. Our focus is on creating the best possible experiences for our community, and we believe that if we do that well, profits will follow. We will never sacrifice our values for short-term gains.",
+      "We believe the key to success is innovation. We don't create products just to make money; we make money to build better products. Our focus is on creating the best possible experiences for our community, and we believe that if we do that well, profits will follow. We will never sacrifice our values for short-term gains.",
     icon: "pound-sign",
     color: "purple",
   },
