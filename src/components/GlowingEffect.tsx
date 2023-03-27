c1de13a5de510db6658c0c1ac8f8c07c1799cb49
David <66641216+fhswno@users.noreply.github.com>
2023-03-27 01:08:38 +0100

    The glowing effect now supports light mode

--- src/components/GlowingEffect.tsx
+++ src/components/GlowingEffect.tsx
@@ -1,6 +1,6 @@
 const GlowingEffect = () => {
   return (
-    <div className="bg-black h-96 flex items-center justify-center w-full">
+    <div className="h-96 flex items-center justify-center w-full">
       <div className="w-32 h-32 bg-blue-500 glowing-effect"></div>
     </div>
   );
