d4e4fdd98fbf9235f31114b96e9e65c1a528ce3a
David <66641216+fhswno@users.noreply.github.com>
2023-03-28 17:08:19 +0100

    Fixed gradient color issue, added a subtle 3D tilt effect

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   FaCodeBranch,
   FaLightbulb,
@@ -28,49 +28,49 @@ const handleIconSelection = (icon: string) => {
     case "code-branch":
       return (
         <FaCodeBranch
-          className="text-black dark:text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
+          className="text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
           style={{ animation: "spin 2s linear infinite" }}
         />
       );
     case "lightbulb":
       return (
         <FaLightbulb
-          className="text-black dark:text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
+          className="text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
           style={{ animation: "spin 2s linear infinite" }}
         />
       );
     case "paint-brush":
       return (
         <FaPaintBrush
-          className="text-black dark:text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
+          className="text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
           style={{ animation: "spin 2s linear infinite" }}
         />
       );
     case "lock":
       return (
         <FaLock
-          className="text-black dark:text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
+          className="text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
           style={{ animation: "spin 2s linear infinite" }}
         />
       );
     case "pound-sign":
       return (
         <FaPoundSign
-          className="text-black dark:text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
+          className="text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
           style={{ animation: "spin 2s linear infinite" }}
         />
       );
     case "users":
       return (
         <FaUsers
-          className="text-black dark:text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
+          className="text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
           style={{ animation: "spin 2s linear infinite" }}
         />
       );
     default:
       return (
         <FaCodeBranch
-          className="text-black dark:text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
+          className="text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mr-2"
           style={{ animation: "spin 2s linear infinite" }}
         />
       );
@@ -80,116 +80,74 @@ const handleIconSelection = (icon: string) => {
 const handleColor = (color: string) => {
   switch (color) {
     case "blue":
-      return {
-        gradientStart: "#0A1A35",
-        gradientEnd: "#0D0E27",
-        border: "#232D47",
-        shadow: "#2A2B7C",
-      };
+      return "from-[#18397C] to-[#1F489E] border-[#00A9FF] shadow-[#2A2B7C]";
     case "red":
-      return {
-        gradientStart: "#3B0D0C",
-        gradientEnd: "#200B1C",
-        border: "#432D38",
-        shadow: "#4F243B",
-      };
+      return "from-[#7A1C1A] to-[#BE2D2D] border-[#E99DC3] shadow-[#4F243B]";
     case "green":
-      return {
-        gradientStart: "#0B1215",
-        gradientEnd: "#0A1E1A",
-        border: "#1F2627",
-        shadow: "#153F37",
-      };
+      return "from-[#2C5437] to-[#3D744C] border-[#0EA100] shadow-[#153F37]";
     case "yellow":
-      return {
-        gradientStart: "#201E0D",
-        gradientEnd: "#151412",
-        border: "#2C281F",
-        shadow: "#3E3720",
-      };
+      return "from-[#625C28] to-[#938A3C] border-[#E1C600] shadow-[#3E3720]";
     case "purple":
-      return {
-        gradientStart: "#1B0A2A",
-        gradientEnd: "#0D0E27",
-        border: "#2C1F47",
-        shadow: "#2A2B7C",
-      };
+      return "from-[#411B6F] to-[#5A269A] border-[#BA00E1] shadow-[#2A2B7C]";
     case "orange":
-      return {
-        gradientStart: "#2A0D0D",
-        gradientEnd: "#1E0A1E",
-        border: "#3D2C3D",
-        shadow: "#4F243B",
-      };
+      return "from-[#704323] to-[#A46334] border-[#FF6D00] shadow-[#4F243B]";
     default:
-      return {
-        gradientStart: "#0A1A35",
-        gradientEnd: "#0D0E27",
-        border: "#232D47",
-        shadow: "#2A2B7C",
-      };
+      return "from-[#0A1A35] to-[#0D0E27] border-[#00A9FF] shadow-[#2A2B7C]";
   }
 };
 
-const CardComponent = ({
-  title,
-  description,
-  icon,
-  color,
-}: CardComponentProps) => {
-  const [style, setStyle] = useState({});
+const Card = ({ title, description, icon, color }: CardComponentProps) => {
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
+  const [hovered, setHovered] = useState(false);
 
   const colorSet = handleColor(color);
+  const cardRef = useRef<HTMLDivElement>(null);
 
-  const handleDragStart = (e: any) => {
-    const cardRect = e.target.getBoundingClientRect();
-    const offsetX = e.clientX - cardRect.left;
-    const offsetY = e.clientY - cardRect.top;
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
 
-    const handleMouseMove = (e: any) => {
-      const dx = e.clientX - (cardRect.left + offsetX);
-      const dy = e.clientY - (cardRect.top + offsetY);
+  const handleMouseLeave = () => {
+    setHovered(false);
+    setRotateX(0);
+    setRotateY(0);
+  };
 
-      const skewX = Math.sign(dx) * Math.min(Math.abs(dx) / 20, 10);
-      const skewY = Math.sign(dy) * Math.min(Math.abs(dy) / 20, 10);
-
-      setStyle({
-        transform: `scale(1.05) skew(${skewY}deg, ${skewX}deg)`,
-        transition: "transform 300ms ease-in-out",
-      });
-    };
-
-    const handleMouseUp = () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
-      setStyle({
-        transform: "scale(1.05)",
-        transition: "transform 300ms ease-in-out",
-      });
-    };
-
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    const { clientX, clientY } = event;
+    const rect = cardRef.current?.getBoundingClientRect();
+    const centerX = rect ? rect.width / 2 : 0;
+    const centerY = rect ? rect.height / 2 : 0;
+    const angleX = (clientY - rect!.top - centerY) / 40;
+    const angleY = -(clientX - rect!.left - centerX) / 20;
+    setRotateX(angleX);
+    setRotateY(angleY);
   };
 
   return (
     <div
-      className={`card bg-gradient-to-r from-[${colorSet.gradientStart}] to-[${colorSet.gradientEnd}] p-6 text-white rounded-[20px] desktop:w-1/4 h-fit m-4 tablet:w-2/6 mobile:w-full shadow-sm shadow-[${colorSet.shadow}] border-[1px] border-[${colorSet.border}] hover:cursor-pointer`}
-      style={style}
-      onMouseDown={handleDragStart}
-      onMouseUp={() => setStyle({})}
-      onMouseLeave={() => setStyle({})}
+      ref={cardRef}
+      className={`${sourceCodePro.className} card bg-gradient-to-r p-6 text-white rounded-[20px] desktop:w-1/4 h-fit m-4 tablet:w-2/6 mobile:w-full drop-shadow-2xl border-[1px] hover:cursor-pointer ${colorSet}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onMouseMove={handleMouseMove}
+      style={{
+        transform: `perspective(1000px) rotateX(${
+          hovered ? rotateX : 0
+        }deg) rotateY(${hovered ? rotateY : 0}deg)`,
+      }}
     >
       <div className="flex flex-row items-center mb-2">
         {handleIconSelection(icon)}
         <h2
-          className={`${sourceCodePro.className} text-2xl font-bold text-black dark:text-white`}
+          className={`${sourceCodePro.className} text-2xl font-bold text-white`}
         >
           {title}
         </h2>
       </div>
       <p
-        className={`${sourceCodePro.className} text-sm font-normal text-black dark:text-white`}
+        className={`${sourceCodePro.className} text-sm font-normal text-white`}
       >
         {description}
       </p>
@@ -197,4 +155,4 @@ const CardComponent = ({
   );
 };
 
-export default CardComponent;
+export default Card;
