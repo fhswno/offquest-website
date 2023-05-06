76732def773d1e175c758494469d4495143a6640
David <66641216+fhswno@users.noreply.github.com>
2023-05-06 17:36:09 +0100

    New format, comments, font and privacy policy in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-// Dependencies & Core Components
+// NEXT
 import Image from "next/image";
-import { Source_Code_Pro } from "next/font/google";
 import Head from "next/head";
+import Link from "next/link";
+
+// HARD CODED DATA
 import { CardData } from "@/data";
 
-// import icon from react-icons
+// ICONS
 import { FaLinkedin, FaTwitter } from "react-icons/fa";
 
-// Components
+// COMPONENTS
 import GlowingEffect from "@/components/GlowingEffect";
 import GradientText from "@/components/GradientText";
 import Card from "@/components/Card";
 
-// Styles
+// STYLES
 import "../../public/glowing-effect.css";
 import "../app/globals.css";
 
-const sourceCodePro = Source_Code_Pro({
-  subsets: ["latin"],
-  variable: "--font-source-code-pro",
-});
+// FONTS
+import { workSans } from "../../lib/fonts/workSans";
 
 export default function Home() {
   return (
@@ -37,16 +37,14 @@ export default function Home() {
               width={40}
               height={40}
               priority
-              // add an animation so the logo gets bigger and glows
               className="hover:animate-pulse"
             />
             <h1
-              className={`${sourceCodePro.className} text-xl font-semibold text-black dark:text-white ml-2`}
+              className={`${workSans.className} text-xl font-semibold text-black dark:text-white ml-2`}
             >
               OffQuest
             </h1>
           </div>
-          {/* Add more navigation items as needed */}
           <div className="flex flex-row items-center">
             <a href="https://www.linkedin.com/company/offquest" target="_blank">
               <FaLinkedin className="text-black dark:text-white text-2xl hover:text-blue-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" />
@@ -59,7 +57,7 @@ export default function Home() {
       </header>
       <main className="py-20 flex flex-col items-center justify-center bg-white dark:bg-black">
         <h2
-          className={`${sourceCodePro.className} text-center text-5xl font-bold text-black dark:text-white mb-8`}
+          className={`${workSans.className} text-center text-5xl font-bold text-black dark:text-white mb-8`}
         >
           Welcome to{" "}
           <span className="hover:animate-pulse hover:cursor-pointer">
@@ -77,7 +75,7 @@ export default function Home() {
         <div className="w-full flex desktop:flex-row mobile:flex-col tablet:flex-row smallMobile:flex-col items-start desktop:h-96 mobile:h-fit tablet:h-fit smallMobile:h-fit desktop:p-20 tablet:p-20 mobile:p-14 smallMobile:p-10">
           <div className="flex flex-col desktop:w-1/2 h-full tablet:w-1/2 mobile:w-full desktop:mb-0 tablet:mb-0 mobile:mb-0 smallMobile:mb-20">
             <h2
-              className={`${sourceCodePro.className} text-4xl text-black dark:text-white`}
+              className={`${workSans.className} text-4xl text-black dark:text-white`}
             >
               Follow us on our{" "}
               <GradientText
@@ -86,14 +84,14 @@ export default function Home() {
               ></GradientText>
             </h2>
             <p
-              className={`${sourceCodePro.className} my-6 text-black dark:text-white`}
+              className={`${workSans.className} my-6 text-black dark:text-white`}
             >
               We are a team of innovators, on a quest to build leading tech
               products, which will dramatically change the way people interact
               with the world around them.
             </p>
             <p
-              className={`${sourceCodePro.className} mb-6 text-black dark:text-white`}
+              className={`${workSans.className} mb-6 text-black dark:text-white`}
             >
               Our goal is to solve real world problems, by creating
               beautifully-crafted products that are easy to use, great to look
@@ -127,7 +125,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col desktop:w-1/2 h-full  tablet:w-1/2 mobile:w-full mobile:order-1 smallMobile:order-1 desktop:order-1 tablet:order-1 ">
             <h2
-              className={`${sourceCodePro.className} text-start text-4xl text-black dark:text-white`}
+              className={`${workSans.className} text-start text-4xl text-black dark:text-white`}
             >
               We are currently{" "}
               <GradientText
@@ -136,14 +134,14 @@ export default function Home() {
               ></GradientText>
             </h2>
             <p
-              className={`${sourceCodePro.className} my-6 text-black dark:text-white`}
+              className={`${workSans.className} my-6 text-black dark:text-white`}
             >
               This product will provide a game-changing solution to a problem
               everyone faces, and will be available to the public in the coming
               months.
             </p>
             <p
-              className={`${sourceCodePro.className} mb-6 text-black dark:text-white`}
+              className={`${workSans.className} mb-6 text-black dark:text-white`}
             >
               Our diverse team of developers, designers and experts are building
               a community-driven product, which will get people together, help
@@ -153,7 +151,7 @@ export default function Home() {
           </div>
         </div>
         <h2
-          className={`${sourceCodePro.className} text-center text-5xl font-bold text-black dark:text-white mb-8 mt-20`}
+          className={`${workSans.className} text-center text-5xl font-bold text-black dark:text-white mb-8 mt-20`}
         >
           <span className="hover:animate-pulse hover:cursor-pointer">
             <GradientText text="Our Values" size="text-5xl" />
@@ -171,7 +169,7 @@ export default function Home() {
           ))}
         </div>
         <h2
-          className={`${sourceCodePro.className} text-center text-5xl font-bold text-black dark:text-white my-8 p-20`}
+          className={`${workSans.className} text-center text-5xl font-bold text-black dark:text-white my-8 p-20`}
         >
           The Quest starts in{" "}
           <span className="hover:animate-pulse hover:cursor-pointer">
@@ -179,17 +177,28 @@ export default function Home() {
           </span>
         </h2>
       </main>
-      <footer className="bg-gray-300 dark:bg-black py-6 px-4 sm:px-6 lg:px-8">
-        <div
-          className={`${sourceCodePro.className} text-center text-gray-600 text-sm`}
+      <footer className="bg-gray-300 dark:bg-black py-6 px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center">
+        <p
+          className={`${workSans.className} text-center text-gray-600 text-sm`}
         >
           Built with ❤️ by OffQuest Team in London, using Next.js & Tailwind CSS
-        </div>
-        <div
-          className={`${sourceCodePro.className} text-center text-gray-600 text-sm`}
+        </p>
+        <p
+          className={`${workSans.className} text-center text-gray-600 text-sm my-4 w-full tablet:w-1/2`}
         >
-          &copy; {new Date().getFullYear()} OffQuest. All rights reserved.
-        </div>
+          &copy; {new Date().getFullYear()} - All rights reserved. OffQuest is a
+          French Societe par Actions Simplifiee (SAS) registered in Paris,
+          France under company number 952107431, whose legal trading name is
+          Caion.
+        </p>
+        <Link
+          href="https://www.notion.so/caion/Privacy-Policy-1957ad2f3f0b4e0dbdecbe9a3096b6ff?pvs=4"
+          passHref
+          target="_blank"
+          className={`${workSans.className} text-center text-gray-600 text-sm hover:text-purple-500`}
+        >
+          Privacy Policy
+        </Link>
       </footer>
     </div>
   );
