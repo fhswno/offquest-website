7e5d4b83b32486495a718df91e4baf5c1af59b5d
David <66641216+fhswno@users.noreply.github.com>
2023-05-06 17:52:14 +0100

    Setup to support eslint, prettier and GitHub actions; added two scripts to handle formatting package.json.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
     extend: {
       // Add custom theme configurations here
       fontFamily: {
-        "source-code-pro": ["Source Code Pro", "monospace"],
+        'source-code-pro': ['Source Code Pro', 'monospace'],
       },
       screens: {
-        smallMobile: "320px",
+        smallMobile: '320px',
 
-        mobile: "550px",
+        mobile: '550px',
 
-        tablet: "840px",
+        tablet: '840px',
 
-        desktop: "1076px",
+        desktop: '1076px',
       },
     },
   },
