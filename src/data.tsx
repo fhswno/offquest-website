34d77def0ac3192425debc8a78739ef0627d9821
David <66641216+fhswno@users.noreply.github.com>
2023-05-06 17:52:44 +0100

    Formatting on data.tsx

--- src/data.tsx
+++ src/data.tsx
@@ -1,44 +1,44 @@
 export const CardData = [
   {
-    title: "Open Source",
+    title: 'Open Source',
     description:
       "We believe that open source is key to unlocking innovation, that's why we embrace open-source technology, making our products accessible to anyone willing to contribute.",
-    icon: "code-branch",
-    color: "blue",
+    icon: 'code-branch',
+    color: 'blue',
   },
   {
-    title: "Disruptive Innovation",
+    title: 'Disruptive Innovation',
     description:
       "We don't just create new products. We redefine entire industries. We think outside the box, dare everything, test our hypotheses, perform extensive user research to solve real, complex problems.",
-    icon: "lightbulb",
-    color: "red",
+    icon: 'lightbulb',
+    color: 'red',
   },
   {
-    title: "Beautiful Design",
+    title: 'Beautiful Design',
     description:
-      "We approach design with the same level of care and attention to detail as we do with our technology. Our products are not only beautiful, but also effortless and intuitive, providing our community with a delightful experience, that is both functional and enjoyable.",
-    icon: "paint-brush",
-    color: "green",
+      'We approach design with the same level of care and attention to detail as we do with our technology. Our products are not only beautiful, but also effortless and intuitive, providing our community with a delightful experience, that is both functional and enjoyable.',
+    icon: 'paint-brush',
+    color: 'green',
   },
   {
-    title: "Trusted & Transparent",
+    title: 'Trusted & Transparent',
     description:
-      "We believe that technology should be trustworthy, reliable and secure. We follow the most stringent security protocols, take user privacy very seriously, and disclose all of our processes and practices in a transparent manner.",
-    icon: "lock",
-    color: "yellow",
+      'We believe that technology should be trustworthy, reliable and secure. We follow the most stringent security protocols, take user privacy very seriously, and disclose all of our processes and practices in a transparent manner.',
+    icon: 'lock',
+    color: 'yellow',
   },
   {
-    title: "Sound Financials",
+    title: 'Sound Financials',
     description:
       "We believe the key to success is innovation. We don't create products just to make money; we make money to build better products. Our focus is on creating the best possible experiences for our community, and we believe that if we do that well, profits will follow. We will never sacrifice our values for short-term gains.",
-    icon: "pound-sign",
-    color: "purple",
+    icon: 'pound-sign',
+    color: 'purple',
   },
   {
-    title: "Community Driven",
+    title: 'Community Driven',
     description:
-      "We believe that our community is the most important part of our business. We are committed to building a community of like-minded individuals, who share our values and are passionate about our mission. We are always looking for new ways to engage with our community, and we are always open to feedback and suggestions.",
-    icon: "users",
-    color: "orange",
+      'We believe that our community is the most important part of our business. We are committed to building a community of like-minded individuals, who share our values and are passionate about our mission. We are always looking for new ways to engage with our community, and we are always open to feedback and suggestions.',
+    icon: 'users',
+    color: 'orange',
   },
 ];
