d640849b63afd023f0210419aba4e928ab71f167
David <66641216+fhswno@users.noreply.github.com>
2024-04-13 18:56:39 +0100

    Update social media links and styling in Branding/Social component

--- src/components/Branding/Social.tsx
+++ src/components/Branding/Social.tsx
@@ -1,15 +1,20 @@
 // NEXT
 import Link from 'next/link';
 
-// COMPONENTS
-import XLogo from '../XLogo';
+// LUCIDE
+import { GitHub } from 'react-feather';
 
 const Social = () => {
   return (
-    <div className="flex flex-row items-center">
-      <Link href="https://x.com/offquesthq" target="_blank">
-        <div className="w-6 h-6 ml-4 text-2xl text-white transition duration-500 ease-in-out transform hover:text-blue-500 hover:-translate-y-1 hover:scale-110 group">
-          <XLogo color="text-white group-hover:text-[#26a7de]" />
+    <div className="flex flex-row items-center gap-1">
+      <Link href="https://x.com/offquesthq" target="_blank" className="p-2">
+        <div className="px-2 ml-4 text-2xl text-white transition duration-500 ease-in-out transform rounded-lg hover:bg-black hover:-translate-y-1 hover:scale-110">
+          <span className="text-4xl">𝕏</span>
+        </div>
+      </Link>
+      <Link href="https://github.com/offquest" target="_blank">
+        <div className="p-2 text-2xl text-white transition duration-500 ease-in-out transform rounded-full hover:-translate-y-1 hover:scale-110 hover:bg-white hover:text-black">
+          <GitHub width={24} height={24} />
         </div>
       </Link>
     </div>
