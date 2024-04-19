fb16bffe37278f2ddb4aff55109962a3ffd6e41a
David <66641216+fhswno@users.noreply.github.com>
2024-04-20 00:02:01 +0100

    Remove unused import and add spacing before main element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 // NEXT
 import Image from 'next/image';
-import Head from 'next/head';
 import Link from 'next/link';
 
 // HARD CODED DATA
@@ -29,6 +28,7 @@ export default function Home() {
       <Ventures />
       <Software />
       <Contact />
+
       <main className="flex flex-col items-center justify-center py-20 bg-white dark:bg-black">
         <h2 className="mb-8 text-5xl font-bold text-center text-black dark:text-white">
           Welcome to{' '}
