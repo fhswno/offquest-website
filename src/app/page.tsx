94e9bd684cac41a652f8781b7197c519aac9111f
David <66641216+fhswno@users.noreply.github.com>
2024-04-20 00:21:32 +0100

    Remove unnecessary footer code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,24 +30,6 @@ export default function Home() {
       <Software />
       <Contact />
       <Footer />
-      <footer className="flex flex-col items-center justify-center px-4 py-6 bg-gray-300 dark:bg-black sm:px-6 lg:px-8">
-        <p className="text-sm text-center text-gray-600">
-          Built with ❤️ in London, using Next.js & Tailwind CSS
-        </p>
-        <p className="w-full my-4 text-sm text-center text-gray-600 tablet:w-1/2">
-          &copy; {new Date().getFullYear()} - All rights reserved. OffQuest is a
-          Simplified Joint Stock Company (SAS) registered in Paris, France under
-          company number 952107431.
-        </p>
-        <Link
-          href="https://sarsburydarwell.notion.site/Privacy-Policy-afa1856985e54b9899cc8776f79f31eb?pvs=4"
-          passHref
-          target="_blank"
-          className="text-sm text-center text-gray-600 hover:text-purple-500"
-        >
-          Privacy Policy
-        </Link>
-      </footer>
     </div>
   );
 }
