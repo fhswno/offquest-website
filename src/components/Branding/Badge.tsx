c564519e7a9367c0ea8c57437ea6022fae4cfaa6
David <66641216+fhswno@users.noreply.github.com>
2024-04-19 02:06:52 +0100

    Refactor Badge component to include shadow color option

--- src/components/Branding/Badge.tsx
+++ src/components/Branding/Badge.tsx
@@ -1,14 +1,30 @@
 // NEXT
 import Image from 'next/image';
 
+// CLSX
+import clsx from 'clsx';
+
+// ENUMS
+import { ShadowColor } from '../../../enums/branding-enums';
+
 // TYPESCRIPT
 type Props = {
   text?: string;
+  shadowColor?: ShadowColor;
 };
 
-const Badge = ({ text = 'OffQuest' }: Props) => {
+const Badge = ({
+  text = 'OffQuest',
+  shadowColor = ShadowColor.BLUE,
+}: Props) => {
   return (
-    <div className="flex items-center justify-center gap-4 p-4 text-2xl bg-gray-700 border border-gray-500 shadow-md rounded-xl w-fit shadow-blue-600">
+    <div
+      className={clsx(
+        'flex items-center justify-center gap-4 p-4 text-2xl bg-gray-700 border border-gray-500 shadow-md rounded-xl w-fit',
+        shadowColor === 'blue' && 'shadow-blue-600',
+        shadowColor === 'green' && 'shadow-green-600',
+      )}
+    >
       <Image
         src="/logo.png"
         alt="OffQuest Logo"
