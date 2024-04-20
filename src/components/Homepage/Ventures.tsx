54d84e8318410f4d21ab8a4a552668a20e7b73fa
David <66641216+fhswno@users.noreply.github.com>
2024-04-20 23:42:41 +0200

    Update Ventures component styling

--- src/components/Homepage/Ventures.tsx
+++ src/components/Homepage/Ventures.tsx
@@ -11,13 +11,13 @@ const Ventures = () => {
       id="ventures"
     >
       <Badge text="Ventures" />
-      <div className="flex flex-col items-center justify-center w-2/3 gap-12">
-        <h2 className="text-4xl leading-tight text-center">
+      <div className="flex flex-col items-center justify-center w-full gap-12 tablet:w-3/4 laptop:w-2/3">
+        <h2 className="text-3xl leading-tight text-center laptop:text-4xl">
           OffQuest Ventures builds, maintains and scales{' '}
           <span className="z-10 underline-effect">forward-thinking</span>{' '}
           FinTech products.
         </h2>
-        <p className="text-lg leading-loose text-center">
+        <p className="text-base leading-loose text-center laptop:text-lg">
           All of our products aim to make Financial Services more transparent,
           accessible and secure for everyone. We are firm believers that
           technology-enabled solutions can drive positive changes, and our
@@ -30,7 +30,7 @@ const Ventures = () => {
           width={800}
           height={800}
           priority
-          className="border border-blue-500 rounded-xl"
+          className="w-4/5 border border-blue-500 rounded-xl laptop:w-full"
           blurDataURL="/ventures.png"
           placeholder="blur"
         />
