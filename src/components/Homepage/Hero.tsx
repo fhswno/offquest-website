c47eda28ee848af93810c3544cda8377ca02f97a
David <66641216+fhswno@users.noreply.github.com>
2024-04-20 23:39:58 +0200

    Update Hero component styles

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -12,12 +12,12 @@ const Hero = () => {
       }}
       className="flex flex-col items-start justify-start w-full h-screen gap-10 p-10 pt-40 text-white"
     >
-      <h1 className="w-2/3 text-5xl leading-tight">
+      <h1 className="w-full text-4xl leading-tight tablet:w-3/4 laptop:text-5xl laptop:w-2/3">
         We&apos;re making{' '}
         <span className="z-10 underline-effect">top-tier</span> Software
         accessible to <span className="z-10 underline-effect">everyone</span>.
       </h1>
-      <p className="w-1/2 text-lg">
+      <p className="w-3/4 p-2 text-base bg-opacity-50 border border-gray-400 tablet:p-0 tablet:bg-opacity-0 tablet:border-none tablet:w-1/2 backdrop-blur-2xl rounded-xl laptop:text-lg">
         OffQuest is a software company specialised in FinTech, on a quest to
         build unique, life-changing products to help people make better
         financial decisions.
