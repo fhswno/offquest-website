2a9f126229a794b4aecb3e19154906f468eafa6b
David <66641216+fhswno@users.noreply.github.com>
2024-04-21 00:05:17 +0200

    Update Contact component styles

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,12 +4,12 @@ import ContactForm from './ContactForm';
 const Contact = () => {
   return (
     <section className="flex flex-col items-center justify-center gap-10 px-10 text-white bg-gray-900 py-28">
-      <div className="flex flex-col items-center justify-center w-1/2 gap-12">
-        <h2 className="text-6xl leading-tight text-center">
+      <div className="flex flex-col items-center justify-center w-full gap-12 tablet:w-2/3 laptop:w-1/2">
+        <h2 className="text-4xl leading-tight text-center tablet:text-5xl laptop:text-6xl">
           {' '}
           <span className="z-10 underline-effect">Talk to us.</span>
         </h2>
-        <p className="text-lg leading-loose text-center">
+        <p className="text-sm leading-loose text-center tablet:text-base laptop:text-lg">
           If you want to contribute to our open-source projects, discuss a
           partnership with one of our ventures, or simply say hi, we&apos;re
           here for you. Just drop your info and we&apos;ll get back to you.
