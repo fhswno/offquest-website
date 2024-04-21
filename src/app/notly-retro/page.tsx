11ff3f03a1cb1789e8b38fd36c9589e65e598a4f
David <66641216+fhswno@users.noreply.github.com>
2024-04-21 20:18:11 +0200

    Add NotlyNavbar component

--- src/app/notly-retro/page.tsx
+++ src/app/notly-retro/page.tsx
@@ -1,3 +1,7 @@
+// NEXT
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Page = () => {
   return (
     <main
@@ -9,9 +13,39 @@ const Page = () => {
         backgroundRepeat: 'no-repeat',
       }}
     >
+      <NotlyNavbar />
       <h1 className="text-4xl">Hello, World!!! 🇮🇱</h1>
     </main>
   );
 };
 
 export default Page;
+
+const NotlyNavbar = () => {
+  return (
+    <nav className="flex flex-row items-center justify-between w-full p-4 text-white ">
+      <Image
+        src="/notly-wordmark.png"
+        alt="Notly Logo"
+        width={300}
+        height={300}
+        priority
+        className="w-44 hover:animate-pulse"
+      />
+      <div className="flex flex-row items-center gap-12">
+        <Link
+          href="/notly-retro/privacy"
+          className="px-4 py-2 text-xl duration-500 ease-in-out rounded-lg hover:bg-orange-800"
+        >
+          Privacy
+        </Link>
+        <Link
+          href="/"
+          className="px-4 py-2 text-xl duration-500 ease-in-out rounded-lg hover:bg-orange-800"
+        >
+          Home
+        </Link>
+      </div>
+    </nav>
+  );
+};
