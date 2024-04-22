31f9ed1c1685c06d9116afb7bf801ea6f6e14f3c
David <66641216+fhswno@users.noreply.github.com>
2024-04-23 00:44:24 +0200

    Add NotlyNavbar component to privacy page

--- src/app/notly/privacy/page.tsx
+++ src/app/notly/privacy/page.tsx
@@ -1,6 +1,10 @@
+// COMPONENTS
+import NotlyNavbar from '@/components/Notly/NotlyNavbar';
+
 const Page = () => {
   return (
-    <main>
+    <main className="from-[#B98662] to-[#8B4513] bg-gradient-to-t flex flex-col items-center justify-start h-screen text-white p-10">
+      <NotlyNavbar isPrivacy={true} />
       <h1>Privacy</h1>
     </main>
   );
