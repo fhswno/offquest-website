127d52c977eddf84fbed3c27a4629c2649f1631a
David <66641216+fhswno@users.noreply.github.com>
2024-04-23 00:44:20 +0200

    Refactor NotlyNavbar component to accept isPrivacy prop

--- src/components/Notly/NotlyNavbar.tsx
+++ src/components/Notly/NotlyNavbar.tsx
@@ -2,7 +2,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const NotlyNavbar = () => {
+// TYPESCRIPT
+type Props = {
+  isPrivacy?: boolean;
+};
+
+const NotlyNavbar = ({ isPrivacy = false }: Props) => {
   return (
     <nav className="flex flex-row items-center justify-between w-full p-4 text-white">
       <Image
@@ -15,10 +20,11 @@ const NotlyNavbar = () => {
       />
       <div className="flex flex-row items-center gap-2 tablet:gap-12">
         <Link
-          href="/notly/privacy"
+          // href="/notly/privacy"
+          href={isPrivacy ? '/notly' : '/notly/privacy'}
           className="px-4 py-2 text-base duration-500 ease-in-out rounded-lg tablet:text-lg laptop:text-xl hover:bg-orange-800"
         >
-          Privacy
+          {isPrivacy ? 'App' : 'Privacy'}
         </Link>
         <Link
           href="/"
