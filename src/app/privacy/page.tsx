ed6606c8fb9131db3285cbcd097efcaeea186062
David <66641216+fhswno@users.noreply.github.com>
2024-04-23 13:46:27 +0200

    Add privacy policy section to page

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,5 +1,6 @@
 // NEXT
 import Image from 'next/image';
+import Link from 'next/link';
 
 // NAVIGATION
 import Navbar from '@/components/Navigation/Navbar';
@@ -21,6 +22,41 @@ const Page = () => {
         />
         <h1 className="text-6xl">Privacy</h1>
       </div>
+      <section className="flex flex-col w-full gap-8 pb-40 text-base tablet:text-lg tablet:w-11/12">
+        <p>
+          <strong>Effective Date</strong>: 23 April 2024
+        </p>
+        <p>
+          offquest.io <i>(hereinafter referred to as the "Website") </i>is a
+          website operated by OffQuest, a Simplified Joint-Stock Company with a
+          share capital of 500 euros and registered office at 10 rue de
+          Penthièvre, 75008 Paris (France), registered with the Paris Trade and
+          Companies Register under the identification number{' '}
+          <Link
+            href="https://annuaire-entreprises.data.gouv.fr/entreprise/952107431?redirected=1"
+            target="_blank"
+            className="underline"
+          >
+            952 107 431
+          </Link>{' '}
+          <i>(the "Company")</i>.
+        </p>
+        <p>
+          Website visitors <i>(the "Visitor(s)")</i> have access to visionary
+          statements and elementary information about the Company.
+        </p>
+        <p>
+          Visiting the website involves the collection of basic, non-sensitive
+          information, such as the Operating System and device used to visit the
+          website, and the country from which you accessed the website.
+          Protecting your data is an absolute priority for the Company, which
+          has adopted the following Privacy Policy to preserve visitors’ trust.
+        </p>
+        <p>
+          All of this information is processed in accordance with this Privacy
+          Policy and is subject to our Terms of Use.
+        </p>
+      </section>
     </main>
   );
 };
