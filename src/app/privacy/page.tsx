b8f46245680d65b75841ee7dcd865dcfb03e88f1
David <66641216+fhswno@users.noreply.github.com>
2024-04-23 13:40:38 +0200

    Add privacy page component

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <main className="flex flex-col items-center justify-center w-full h-screen text-white bg-gray-900">
+      <h1>Privacy</h1>
+    </main>
+  );
+};
+
+export default Page;
