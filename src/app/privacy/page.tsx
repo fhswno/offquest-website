00156e97d171f3571bc0f73aad9b648b267f98a6
David <66641216+fhswno@users.noreply.github.com>
2024-04-23 14:01:14 +0200

    Add privacy information collection section

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -56,6 +56,114 @@ const Page = () => {
           All of this information is processed in accordance with this Privacy
           Policy and is subject to our Terms of Use.
         </p>
+        <h2 className="text-2xl font-semibold tablet:text-3xl">
+          Information Collection
+        </h2>
+        <p>
+          To allow Visitors to fully enjoy the services and features of the
+          Website, the Company collects essential, non-sensitive information. It
+          is recalled that by visiting on the Website, you expressly agree that
+          this information be collected regardless of the country from which you
+          are connecting. Furthermore, the fact that you provide basic data to
+          the Company is a completely voluntary step on your part. You have the
+          possibility to oppose this at any time, in accordance with the General
+          Conditions of Use of the Website and this Privacy Policy, by letting
+          us know directly, knowing that the withdrawal of your consent does not
+          operate retroactively.
+        </p>
+        <p className="underline">
+          This information is collected at different times and by various
+          methods:
+        </p>
+        <h3 className="text-xl font-semibold tablet:text-2xl">
+          Information Collected
+        </h3>
+        <p>
+          Information provided during every visit: upon visiting the website, we
+          collect the name of country from which you visited the Website, the
+          device and operating system you used to visit the Website and the
+          page(s) you have visited.
+        </p>
+        <p>
+          We require visitors to provide this data is order to analyse and
+          monitor web traffic, and be able to fix potential bugs or issues that
+          may arise for specific visitors.
+        </p>
+        <p>
+          All data collected is anonymised, and never include sensitive or
+          personal data such as a person’s name, address, age, occupation,
+          interests etc.
+        </p>
+        <h3 className="text-xl font-semibold tablet:text-2xl">
+          Use of the Website
+        </h3>
+        <p>
+          During your use of the Website, the Company's servers automatically
+          record the information created and exchanged using our Services. For
+          example, the Company is aware of all your session information{' '}
+          <i>(which page you visited on the Website, what data was entered)</i>.
+        </p>
+        <h3 className="text-xl font-semibold tablet:text-2xl">
+          Log files, Internet Protocol <i>(IP)</i> address, and Computer
+          Information
+        </h3>
+        <p>
+          When you connect to the Website, the Company receives the link from
+          the site from which you arrived and the one to which you are going
+          when you leave the Website.
+        </p>
+        <p>
+          The Company also receives your internet protocol <i>(IP)</i> address
+          or certain information regarding the operating system of your computer
+          or its internet browser.
+        </p>
+        <h3 className="text-xl font-semibold tablet:text-2xl">Cookies</h3>
+        <p>
+          The Company does not use cookies and doesn’t store any visitor
+          information.
+        </p>
+        <h2 className="text-2xl font-semibold tablet:text-3xl">
+          Retention of Collected Information
+        </h2>
+        <p>
+          Once collected, this information is stored on the servers of the
+          Company or any subcontractor of the Company for the purpose of
+          processing them in connection with the use of the Services. They are
+          kept for no more than thirty days.{' '}
+        </p>
+        <p>
+          You always remain the owner of the information about you that you
+          transmit to the Website. In this respect, you have, in accordance with
+          Law No. 78-17 of January 6, 1978, as amended by Law No. 2004-801 of
+          August 6, 2004 and EU Regulation 2016/679 of April 27, 2016, a right
+          of access, rectification, limitation of processing and/or deletion of
+          personal data concerning you, as well as the right to object to such
+          processing and the right to data portability. To exercise your rights,
+          please write to the following email address:{' '}
+          <Link
+            href="mailto:privacy@offquest.io?subject=About OffQuest's Privacy Policy&body=Hello,%0D%0A%0D%0AI have a question about OffQuest's Privacy Policy..."
+            className="underline"
+          >
+            privacy@offquest.io
+          </Link>{' '}
+          or to the following postal address:{' '}
+        </p>
+        <p>
+          <strong>OffQuest</strong> - 10 rue de Penthièvre, 75008 Paris
+          (France).
+        </p>
+        <p> A response to your request will be sent to you within 30 days.</p>
+        <p>
+          You retain the right to lodge a complaint with the{' '}
+          <Link
+            href="https://www.cnil.fr/en"
+            target="_blank"
+            className="underline"
+          >
+            CNIL
+          </Link>{' '}
+          for infringement of your data protection rights.
+        </p>
       </section>
     </main>
   );
