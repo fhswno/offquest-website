00a3dcf020cdcba59dd41cd0bfa34c39f1098715
David <66641216+fhswno@users.noreply.github.com>
2024-04-23 13:42:27 +0200

    Add logo and navigation to privacy page

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,7 +1,26 @@
+// NEXT
+import Image from 'next/image';
+
+// NAVIGATION
+import Navbar from '@/components/Navigation/Navbar';
+
 const Page = () => {
   return (
     <main className="flex flex-col items-center justify-center w-full h-screen text-white bg-gray-900">
-      <h1>Privacy</h1>
+      <Navbar />
+      <div className="flex flex-row items-center gap-4 h-96">
+        <Image
+          src="/logo.png"
+          alt="OffQuest Logo"
+          width={300}
+          height={300}
+          priority
+          className="w-16"
+          blurDataURL="/logo.png"
+          placeholder="blur"
+        />
+        <h1 className="text-6xl">Privacy</h1>
+      </div>
     </main>
   );
 };
