e1acc4a2a647e94641251a2f902c91f11410e993
David <66641216+fhswno@users.noreply.github.com>
2024-04-25 08:08:27 +0200

    Add loading page component

---
+++ src/app/loading/page.tsx
@@ -0,0 +1,9 @@
+// NEXT
+
+export default function Loading() {
+  return (
+    <main className="flex flex-col items-center justify-center w-full h-screen text-white bg-gray-900">
+      <h1>Loading...</h1>
+    </main>
+  );
+}
